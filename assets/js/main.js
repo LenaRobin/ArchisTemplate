7a73bff2ddbb0f9131eaf25ccd8e31bbf7d54692
Léna <lena@lava.nl>
2021-01-21 16:53:35 +0100

    Big update

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,11 +32,72 @@ document.addEventListener("DOMContentLoaded", function(){
 
 
 	// Filters opening
-	var open = document.querySelector('.button__open');
-	var panel = document.querySelector('.filter__panel');
+	var buttons = document.querySelectorAll('.nav__actions .button', '.filter__actions .button');
+	var filterGroups = document.querySelectorAll('.nav__actions .button .filter__group', '.filter__actions .button .filter__group');
 
-	open.addEventListener('click', function(){
-		panel.classList.add('filter__panel--open');
+
+	buttons.forEach(function(button){
+		button.addEventListener('click', function(){
+			var panel = button.querySelector('.filter__group');
+
+			filterGroups.forEach(function(group){
+				if(group == panel) {
+					panel.classList.toggle('filter__group--open');
+				} else {
+					group.classList.remove('filter__group--open');	
+				}
+			});
+
+			
+			
+		});
 	});
 
+
+
+
+	// Scroll direction
+	var scrollPos = 0;
+	var body = document.querySelector('body');
+    var layers = document.querySelector('.layers');
+	window.addEventListener('scroll', function(direction){
+		// detects new state and compares it with the new one
+		if ((document.body.getBoundingClientRect()).top >= scrollPos) {
+			body.setAttribute('data-scroll-direction', 'up');
+
+			if(body.classList.contains('body--tablet')) {
+				console.log('beep boop');
+				layers.classList.add('layers--down');
+			}
+		} else {
+			body.setAttribute('data-scroll-direction', 'down');
+			layers.classList.remove('layers--down');
+		}
+		scrollPos = (document.body.getBoundingClientRect()).top;
+	});
+
+
+	// Mobile menu showing
+	function responsiveCalculation(){
+	    var newWidth = window.innerWidth;
+	    var newHeight = window.innerHeight; 
+	    var tablet = 924;
+	    var phone = 624;
+
+	    if (newWidth < tablet) {
+	   		body.classList.add('body--tablet');
+
+	   		// if (body.getAttribute('data-scroll-direction') == 'up') {
+	   		// 	console.log('tablet up');
+	   		// }
+	    }
+
+	    if (newWidth > tablet) {
+	   		body.classList.remove('body--tablet');
+	    }
+	}
+
+	window.addEventListener('resize', responsiveCalculation);
+
+	responsiveCalculation();
 });
